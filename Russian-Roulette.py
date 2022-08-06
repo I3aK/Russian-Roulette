d421c90a341faf43e8fc991a07978312f717e668
I3aK <42650949+I3aK@users.noreply.github.com>
2022-08-06 14:43:57 +0300

    class

--- Russian-Roulette.py
+++ Russian-Roulette.py
@@ -6,14 +6,22 @@ WIDTH = 800
 # Color
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
-RED = (50, 150, 50)
-GREEN = (0, 255, 0)
+RED = (150, 50, 50)
+GREEN = (50, 150, 50)
 BLUE = (117, 187, 253)
 GRAYF = (58, 58, 58)
 METAL1 = (222, 224, 223)
 METAL2 = (200, 204, 203)
 
 mid_x = WIDTH // 2
+
+class bullet (pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((mid_x,mid_x - (WIDTH / 32)))
+        self.image.fill(RED)
+        self.rect = self.image.get_rect()
+        self.rect.center = (WIDTH / 2, HEIGHT / 2)
 
 def start_window():
     pygame.draw.rect(screen, RED,(mid_x - (WIDTH / 100), mid_x - (WIDTH / 10), (WIDTH / 100), (WIDTH / 100)))
@@ -23,7 +31,9 @@ def start_window():
     pygame.draw.circle(screen, GRAYF, (mid_x + (WIDTH / 32), mid_x), (WIDTH / 100), width = 2)
     pygame.draw.circle(screen, GRAYF, (mid_x - (WIDTH / 32), mid_x), (WIDTH / 100), width = 2)
     pygame.draw.circle(screen, GRAYF, (mid_x, mid_x + (WIDTH / 32)), (WIDTH / 100), width = 2)
+    #вурхняя точка
     pygame.draw.circle(screen, GRAYF, (mid_x, mid_x - (WIDTH / 32)), (WIDTH / 100), width = 2)
+
     pygame.draw.circle(screen, GRAYF, (mid_x + (WIDTH / 44.4), mid_x - (WIDTH / 44.4)), (WIDTH / 100), width = 2)
     pygame.draw.circle(screen, GRAYF, (mid_x + (WIDTH / 44.4), mid_x + (WIDTH / 44.4)), (WIDTH / 100), width = 2)
     pygame.draw.circle(screen, GRAYF, (mid_x - (WIDTH / 44.4), mid_x - (WIDTH / 44.4)), (WIDTH / 100), width = 2)
