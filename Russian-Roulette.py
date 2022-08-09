70909f56dceb5b7be68d17972f2e76c79a8d2f76
I3aK <42650949+I3aK@users.noreply.github.com>
2022-08-09 20:49:40 +0300

    add shmup

--- Russian-Roulette.py
+++ Russian-Roulette.py
@@ -2,6 +2,7 @@ import pygame
 import random
 
 WIDTH = 800
+FPS = 30
 
 # Color
 WHITE = (255, 255, 255)
@@ -15,19 +16,44 @@ METAL2 = (200, 204, 203)
 
 mid_x = WIDTH // 2
 
+coordinates = {
+    1:(mid_x, mid_x - (WIDTH / 8)),
+    2:(mid_x + (WIDTH / 11), mid_x - (WIDTH / 11)),
+    3:(mid_x + (WIDTH / 8), mid_x),
+    4:(mid_x + (WIDTH / 11), mid_x + (WIDTH / 11)),
+    5:(mid_x, mid_x + (WIDTH / 8)),
+    6:(mid_x - (WIDTH / 11), mid_x + (WIDTH / 11)),
+    7:(mid_x - (WIDTH / 8), mid_x),
+    8:(mid_x - (WIDTH / 11), mid_x - (WIDTH / 11))
+}
+
 class bullet (pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.Surface( ((WIDTH // 23)*2, (WIDTH // 23)*2) )
         self.image.fill(RED)
         self.rect = self.image.get_rect()
-        self.rect.center = (mid_x, mid_x - (WIDTH / 8) )
+        self.rect.center = (coordinates[1] )
+
+    def Left(self, i ):
+        self.rect.x = coordinates[i][1]
+        
+
+
+class tock (pygame.sprite.Sprite):
+    def __init__(self,wid_x):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface( ((WIDTH // 23)*2, (WIDTH // 23)*2) )
+        self.image.fill(GRAYF)
+        self.rect = self.image.get_rect()
+        self.rect.center = (wid_x)
 
 def start_window():
     pygame.draw.rect(screen, GREEN,(mid_x - (WIDTH / 62), mid_x - (WIDTH / 4), (WIDTH / 28), (WIDTH / 16)))
     pygame.draw.circle(screen, METAL1, (mid_x, mid_x), (WIDTH / 5.55))
     pygame.draw.circle(screen, METAL2, (mid_x, mid_x), (WIDTH / 25))
     pygame.draw.circle(screen, BLACK, (mid_x, mid_x), (WIDTH / 50))
+    '''
     pygame.draw.circle(screen, GRAYF, (mid_x + (WIDTH / 8), mid_x), (WIDTH / 23), width = 2) # 3
     pygame.draw.circle(screen, GRAYF, (mid_x - (WIDTH / 8), mid_x), (WIDTH / 23), width = 2) # 7
     pygame.draw.circle(screen, GRAYF, (mid_x, mid_x + (WIDTH / 8)), (WIDTH / 23), width = 2) # 5
@@ -36,15 +62,23 @@ def start_window():
     pygame.draw.circle(screen, GRAYF, (mid_x + (WIDTH / 11), mid_x + (WIDTH / 11)), (WIDTH / 23), width = 2) # 4
     pygame.draw.circle(screen, GRAYF, (mid_x - (WIDTH / 11), mid_x - (WIDTH / 11)), (WIDTH / 23), width = 2) # 8 
     pygame.draw.circle(screen, GRAYF, (mid_x - (WIDTH / 11), mid_x + (WIDTH / 11)), (WIDTH / 23), width = 2) # 6
-
+    '''
 
 
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, WIDTH))
 background_image = pygame.image.load("map.jpg").convert()
+clock = pygame.time.Clock()
 all_sprites = pygame.sprite.Group()
 bullet = bullet()
-all_sprites.add(bullet)
+tock_1 = tock(coordinates[2])
+tock_2 = tock(coordinates[3])
+tock_3 = tock(coordinates[4])
+tock_4 = tock(coordinates[5])
+tock_5 = tock(coordinates[6])
+tock_6 = tock(coordinates[7])
+tock_7 = tock(coordinates[8])
+all_sprites.add(bullet, tock_1, tock_2, tock_3, tock_4, tock_5, tock_6, tock_7)
 
 
 
@@ -52,8 +86,10 @@ running = True
 while running:
     screen.blit(background_image, [0,0])
     start_window()
-    all_sprites.update()
-
+    clock.tick(FPS)
+    bullet.Left(random.randint(1, 8))
+    all_sprites.draw(screen)
+        
 
 
 
